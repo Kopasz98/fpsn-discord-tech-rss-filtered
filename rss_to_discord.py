9b4d674aade9d1a65d0c78acb0343d73e36bdbd6
Kopasz98 <kozmabalazs9813@gmail.com>
2025-12-17 10:05:56 +0100

    Update rss_to_discord.py

--- rss_to_discord.py
+++ rss_to_discord.py
@@ -8,81 +8,81 @@ import hashlib
 WEBHOOK_URL = os.environ["DISCORD_WEBHOOK"]
 
 RSS_FEEDS = [
-"https://techcrunch.com/feed/",
-"https://www.theverge.com/rss/index.xml",
-"https://www.wired.com/feed/rss",
-"https://feeds.arstechnica.com/arstechnica/index",
-"https://www.engadget.com/rss.xml",
-"https://mashable.com/feeds/rss/tech",
-"https://gizmodo.com/rss",
-"https://www.digitaltrends.com/feed/",
-"https://www.zdnet.com/news/rss.xml",
-"https://venturebeat.com/feed/",
-"https://rss.slashdot.org/Slashdot/slashdot",
-"https://www.fastcompany.com/technology/rss",
-"https://www.axios.com/technology/rss",
-"https://www.technologyreview.com/feed/",
-"https://spectrum.ieee.org/rss",
-"https://www.tomsguide.com/feeds/all",
-"https://www.cnet.com/rss/all/",
-"https://www.pcmag.com/feeds/rss",
-"https://www.howtogeek.com/feed/",
-"https://www.extremetech.com/feed",
+    "https://techcrunch.com/feed/",
+	"https://www.theverge.com/rss/index.xml",
+	"https://www.wired.com/feed/rss",
+	"https://feeds.arstechnica.com/arstechnica/index",
+	"https://www.engadget.com/rss.xml",
+	"https://mashable.com/feeds/rss/tech",
+	"https://gizmodo.com/rss",
+	"https://www.digitaltrends.com/feed/",
+	"https://www.zdnet.com/news/rss.xml",
+	"https://venturebeat.com/feed/",
+	"https://rss.slashdot.org/Slashdot/slashdot",
+	"https://www.fastcompany.com/technology/rss",
+	"https://www.axios.com/technology/rss",
+	"https://www.technologyreview.com/feed/",
+	"https://spectrum.ieee.org/rss",
+	"https://www.tomsguide.com/feeds/all",
+	"https://www.cnet.com/rss/all/",
+	"https://www.pcmag.com/feeds/rss",
+	"https://www.howtogeek.com/feed/",
+	"https://www.extremetech.com/feed",
 
-"https://news.ycombinator.com/rss",
-"https://www.infoq.com/feed/",
-"https://www.theregister.com/headlines.atom",
-"https://stackoverflow.blog/feed/",
-"https://dev.to/feed",
-"https://www.smashingmagazine.com/feed/",
-"https://css-tricks.com/feed/",
-"https://dzone.com/feed",
-"https://blog.cloudflare.com/rss/",
-"https://developers.googleblog.com/feeds/posts/default",
+	"https://news.ycombinator.com/rss",
+	"https://www.infoq.com/feed/",
+	"https://www.theregister.com/headlines.atom",
+	"https://stackoverflow.blog/feed/",
+	"https://dev.to/feed",
+	"https://www.smashingmagazine.com/feed/",
+	"https://css-tricks.com/feed/",
+	"https://dzone.com/feed",
+	"https://blog.cloudflare.com/rss/",
+	"https://developers.googleblog.com/feeds/posts/default",
 
-"https://www.artificialintelligence-news.com/feed/",
-"https://www.aitrends.com/feed/",
-"https://syncedreview.com/feed/",
-"https://openai.com/blog/rss/",
-"https://deepmind.google/blog/rss.xml",
-"https://towardsdatascience.com/feed",
-"https://www.kdnuggets.com/feed",
-"https://www.analyticsvidhya.com/feed/",
-"https://machinelearningmastery.com/blog/feed/",
+	"https://www.artificialintelligence-news.com/feed/",
+	"https://www.aitrends.com/feed/",
+	"https://syncedreview.com/feed/",
+	"https://openai.com/blog/rss/",
+	"https://deepmind.google/blog/rss.xml",
+	"https://towardsdatascience.com/feed",
+	"https://www.kdnuggets.com/feed",
+	"https://www.analyticsvidhya.com/feed/",
+	"https://machinelearningmastery.com/blog/feed/",
 
-"https://krebsonsecurity.com/feed/",
-"https://thehackernews.com/feeds/posts/default",
-"https://www.bleepingcomputer.com/feed/",
-"https://www.darkreading.com/rss.xml",
-"https://threatpost.com/feed/",
-"https://www.securityweek.com/rss",
-"https://www.schneier.com/feed/atom/",
+	"https://krebsonsecurity.com/feed/",
+	"https://thehackernews.com/feeds/posts/default",
+	"https://www.bleepingcomputer.com/feed/",
+	"https://www.darkreading.com/rss.xml",
+	"https://threatpost.com/feed/",
+	"https://www.securityweek.com/rss",
+	"https://www.schneier.com/feed/atom/",
 
-"https://www.hwsw.hu/xml/rss.xml",
-"https://prohardver.hu/hirfolyam/rss.html",
-"https://itcafe.hu/hirfolyam/rss.html",
-"https://bitport.hu/rss",
-"https://sg.hu/rss",
-"https://pcworld.hu/rss",
-"https://techkalauz.hu/rss",
-"https://raketa.hu/rss",
-"https://ipon.hu/magazin/rss",
+	"https://www.hwsw.hu/xml/rss.xml",
+	"https://prohardver.hu/hirfolyam/rss.html",
+	"https://itcafe.hu/hirfolyam/rss.html",
+	"https://bitport.hu/rss",
+	"https://sg.hu/rss",
+	"https://pcworld.hu/rss",
+	"https://techkalauz.hu/rss",
+	"https://raketa.hu/rss",
+	"https://ipon.hu/magazin/rss",
 
-"https://www.gsmarena.com/rss-news-reviews.php3",
-"https://www.androidauthority.com/feed",
-"https://9to5google.com/feed/",
-"https://www.macrumors.com/macrumors.xml",
-"https://www.anandtech.com/rss/",
+	"https://www.gsmarena.com/rss-news-reviews.php3",
+	"https://www.androidauthority.com/feed",
+	"https://9to5google.com/feed/",
+	"https://www.macrumors.com/macrumors.xml",
+	"https://www.anandtech.com/rss/"
 
 ]
 
 INCLUDE_KEYWORDS = [k.lower() for k in [
-  "Samsung","ROG","Ally","Leica","Lenovo",
-  "Legion","iOS","Iphone","Ipad",
-  "Apple","watchOS","Steam","Logitech",
-  "Nintendo","Switch","Sony","Playstation",
-  "PS5","WWDC","Corsair","Razer","Windows",
-  "Asus","Huawei","Nvidia","AMD",
+    "Samsung","ROG","Ally","Leica","Lenovo",
+	"Legion","iOS","Iphone","Ipad",
+	"Apple","watchOS","Steam","Logitech",
+	"Nintendo","Switch","Sony","Playstation",
+	"PS5","WWDC","Corsair","Razer","Windows",
+	"Asus","Huawei","Nvidia","AMD"
 ]]
 
 EXCLUDE_KEYWORDS = [k.lower() for k in [
@@ -139,14 +139,9 @@ for feed_url in RSS_FEEDS:
             continue
 
         new_posts.append({
-           title = entry.get("title", "").strip()
-link  = entry.get("link", "").strip()
-
-# Ha nincs cím vagy link, lépjünk tovább (ne dőljön el a script)
-if not title or not link:
-    continue
-
-new_posts.append({"title": title, "link": link})
+            "title": entry.title,
+            "link": entry.link
+        })
 
         posted.add(eid)
 
